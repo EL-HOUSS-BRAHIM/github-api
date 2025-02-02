1e6c8dbea5fb1254f0c9f86e4aedb74cf74d13d6
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-03 00:05:54 +0100

    updating index  file

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+  },
+  githubToken: process.env.GITHUB_TOKEN,
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+};
