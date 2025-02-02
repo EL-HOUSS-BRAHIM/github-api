4fc84ff759e20f8d77bd0d05422fb31b83e24cc9
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-02 23:58:49 +0100

    updating index  file

---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Repository = require('./Repository');
+const Activity = require('./Activity');
+// Import other models if you have them
+
+module.exports = {
+  User,
+  Repository,
+  Activity,
+  // ... other models
+};
