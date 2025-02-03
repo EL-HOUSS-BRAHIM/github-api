7afcd3a19281d99b10fa8c424225c18dd17d0b88
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-03 13:18:12 +0100

    updating database  file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,18 @@
 const { Sequelize } = require('sequelize');
-const config = require('./index');
+const config = require('./index'); // Assuming you have database config in index.js
 
 const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, {
   host: config.db.host,
   dialect: 'mysql',
+  logging: false, // Set to true to see SQL queries (useful for debugging)
+
+  // Pool configuration (important for serverless environments)
+  pool: {
+    max: 5,    // Maximum number of connections in pool
+    min: 0,    // Minimum number of connections in pool
+    acquire: 30000, // Maximum time (ms) that pool will try to get a connection before throwing error
+    idle: 10000,   // Maximum time (ms) that a connection can be idle before being released
+  },
 });
 
 module.exports = sequelize;
