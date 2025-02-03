8cdf5b1b7f5f74b3ce889c45eb6041855c88f937
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-03 13:17:36 +0100

    updating index  file

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,16 @@
 const User = require('./User');
 const Repository = require('./Repository');
 const Activity = require('./Activity');
-// Import other models if you have them
+
+// Define associations here if not already done in individual model files
+User.hasMany(Repository, { foreignKey: 'user_id' });
+Repository.belongsTo(User, { foreignKey: 'user_id' });
+
+User.hasMany(Activity, { foreignKey: 'user_id' });
+Activity.belongsTo(User, { foreignKey: 'user_id' });
 
 module.exports = {
   User,
   Repository,
   Activity,
-  // ... other models
 };
