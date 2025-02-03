e105c5e49b22a627aaec08f0220b37143f5146c5
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-03 13:17:27 +0100

    updating user  file

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
+// Route to get a user's profile
 router.get('/:username', userController.getUserProfile);
+
+// Route to get a user's repositories
 router.get('/:username/repos', userController.getUserRepos);
+
+// Route to get a user's activity
 router.get('/:username/activity', userController.getUserActivity);
+
+// Route to get a user's aggregated report
 router.get('/:username/report', userController.getAggregatedReport);
 
 module.exports = router;
