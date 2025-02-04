db15709e6d6a135a95c3f2be23dd4dbe0c70dddc
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-04 23:57:40 +0100

    updating index  file

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
+const fs = require('fs');
 
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME,
+    ssl: {
+      ca: fs.readFileSync(process.env.DB_SSL_CA_PATH), // Read the CA certificate
+      rejectUnauthorized: true, // Ensure SSL is required
+    },
   },
   githubToken: process.env.GITHUB_TOKEN,
   redis: {
