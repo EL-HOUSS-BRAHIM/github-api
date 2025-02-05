9c9ab68d6993f07c313450155372496bd0e146a2
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-05 19:51:41 +0100

    updating ranking  file

--- src/routes/ranking.js
+++ src/routes/ranking.js
@@ -5,4 +5,7 @@ const rankingController = require('../controllers/rankingController');
 router.post('/calculate/:username', rankingController.calculateUserRanking);
 router.get('/user/:username', rankingController.getUserRanking);
 
+// New route to harvest users by country
+router.post('/harvest/:country', rankingController.harvestUsersByCountry);
+
 module.exports = router;
