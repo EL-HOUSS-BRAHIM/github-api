42f156967f62e414f15e3ecb03f1be1c390527b8
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-05 12:35:01 +0100

    updating scheduler  file

---
+++ src/scheduler.js
@@ -0,0 +1,8 @@
+const cron = require('node-cron');
+const rankingService = require('./services/ranking');
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('Updating user rankings...');
+  await rankingService.updateRankings();
+  console.log('User rankings updated');
+});
