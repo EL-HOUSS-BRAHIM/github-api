2752890867ea5364ddd52d2a4b098128bb98924f
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-08 13:37:12 +0100

    updating ranking file to the new repo stracture

---
+++ my-backend-app/src/routes/ranking.js
@@ -0,0 +1,12 @@
+// src/routes/ranking.js
+const express = require('express');
+const router = express.Router();
+const rankingController = require('../controllers/rankingController');
+
+router.post('/calculate/:username', rankingController.calculateUserRanking);
+router.get('/user/:username', rankingController.getUserRanking);
+
+// New route to harvest users by country
+router.post('/harvest/:country', rankingController.harvestUsersByCountry);
+
+module.exports = router;
