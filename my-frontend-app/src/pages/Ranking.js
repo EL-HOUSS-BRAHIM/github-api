9ced5890f0d809117c5c2fe0f6f21c0f5624ffe0
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-08 16:08:40 +0100

    updating Ranking file

---
+++ my-frontend-app/src/pages/Ranking.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Rankings() {
+  return (
+    <div className="rankings-container animated fadeIn">
+      <h2>Global Developer Rankings</h2>
+      <div className="rankings-grid">
+        {/* Add rankings content here */}
+      </div>
+    </div>
+  );
+}
+
+export default Rankings;
