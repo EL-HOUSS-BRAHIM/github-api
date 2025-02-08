0e2d2cadc4a5cd75287dc961b1823fc5ce39b21b
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-08 13:42:07 +0100

    updating App file to the new repo stracture

---
+++ my-frontend-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import UserCard from './components/UserCard';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user/:username" element={<UserCard />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
