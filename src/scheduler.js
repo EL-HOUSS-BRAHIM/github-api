70ee1c29fd30676c5b673727c10327baf6ae65e6
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-08 13:13:54 +0100

    updating scheduler file to restracture the repo

--- src/scheduler.js
+++
@@ -1,9 +0,0 @@
-// src/scheduler.js
-const cron = require('node-cron');
-const rankingService = require('./services/ranking');
-
-cron.schedule('0 0 * * *', async () => {
-  console.log('Updating user rankings...');
-  await rankingService.updateRankings();
-  console.log('User rankings updated');
-});
