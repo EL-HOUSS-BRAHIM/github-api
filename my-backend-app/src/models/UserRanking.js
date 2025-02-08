ee045123c8977c33090e0e4aec0060725ed79232
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-08 13:37:04 +0100

    updating UserRanking file to the new repo stracture

---
+++ my-backend-app/src/models/UserRanking.js
@@ -0,0 +1,50 @@
+// src/models/UserRanking.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./User');
+
+const UserRanking = sequelize.define('UserRanking', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  score: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  total_commits: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  total_contributions: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  global_rank: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  country_rank: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  last_calculated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  }
+});
+
+User.hasOne(UserRanking, { foreignKey: 'user_id' });
+UserRanking.belongsTo(User, { foreignKey: 'user_id' });
+
+module.exports = UserRanking;
