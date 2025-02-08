13d132ea7208f45c23a9d0b9a716404af6b9754b
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-08 16:08:57 +0100

    updating App file

--- my-frontend-app/src/App.js
+++ my-frontend-app/src/App.js
@@ -1,3 +1,4 @@
+/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
@@ -5,20 +6,27 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import UserCard from './components/UserCard';
+import Rankings from './pages/Rankings';
+import Reports from './pages/Reports';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/user/:username" element={<UserCard />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
+    <div className="app">
+      <canvas id="canvas-bg"></canvas>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/user/:username" element={<UserCard />} />
+            <Route path="/rankings" element={<Rankings />} />
+            <Route path="/reports" element={<Reports />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
