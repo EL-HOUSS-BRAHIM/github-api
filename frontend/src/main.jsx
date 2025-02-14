49241ee9207d0157ccef5b6979c14a094623c56e
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-14 20:44:45 +0100

    updating main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
-import './styles/global.module.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
