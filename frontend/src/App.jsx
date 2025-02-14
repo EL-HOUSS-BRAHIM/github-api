34134ecce561392f901f8f0ade451369063a6931
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-02-14 20:44:25 +0100

    updating App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import UserProfile from './components/UserProfile';
 import Navbar from './components/Navbar';
-import styles from './styles/global.module.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className={styles.container}>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/user/:username" element={<UserProfile />} />
       </Routes>
-    </div>
+      <Footer />
+      </>
   );
 }
 
